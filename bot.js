d306887721a8db50c579d284e352dae3f5fc4d4a
Alexandru Rosianu <me@aluxian.com>
2016-08-03 23:18:05 +0300

    Connect to wit.ai

--- bot.js
+++ bot.js
@@ -1,7 +1,14 @@
 const botBuilder = require('claudia-bot-builder');
 const excuse = require('huh');
+const wit = require('node-wit');
+const Wit = wit.Wit;
+
+const client = new Wit({accessToken: '5YR7SD27AAQNFKR2W63ZETSENKB4T4CT'});
 
 module.exports = botBuilder(function(request) {
-  console.log('req test', request);
-  return 'Thanks for sending ' + request.text + '. Your message is very important to us, but ' + excuse.get();
+  return client.converse(request.sender, request.text, {})
+    .then((data) => {
+      console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+      return JSON.stringify(data);
+    });
 });
