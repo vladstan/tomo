6c1df4bf0407db6ba0a21609cde9b0dca5a4a137
Alexandru Rosianu <me@aluxian.com>
2016-08-03 22:17:41 +0300

    Add console log

--- bot.js
+++ bot.js
@@ -2,5 +2,6 @@ const botBuilder = require('claudia-bot-builder');
 const excuse = require('huh');
 
 module.exports = botBuilder(function(request) {
+  console.log('req test', request);
   return 'Thanks for sending ' + request.text + '. Your message is very important to us, but ' + excuse.get();
 });
