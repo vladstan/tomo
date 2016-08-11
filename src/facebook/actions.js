668990884c29e16f9dbbd94343d91d9356bde4d6
Vlad Stan <vlad.stan@gmail.com>
2016-08-11 15:22:53 +0300

    The guided flow for real estate

--- src/facebook/actions.js
+++ src/facebook/actions.js
@@ -35,6 +35,12 @@ class Actions {
   async realEstate({sessionId, context, entities}) {
     console.info('action:realEstate', ...arguments);
     const re_intent = firstEntityValue(entities, 're_intent'); // eslint-disable-line camelcase
+    const re_property = firstEntityValue(entities, 're_property'); // eslint-disable-line camelcase
+    const re_location = firstEntityValue(entities, 're_location'); // eslint-disable-line camelcase
+    const re_bedrooms = firstEntityValue(entities, 're_bedrooms'); // eslint-disable-line camelcase
+    const re_bathrooms = firstEntityValue(entities, 're_bathrooms'); // eslint-disable-line camelcase
+    const re_budget = firstEntityValue(entities, 're_budget'); // eslint-disable-line camelcase
+    const re_furniture = firstEntityValue(entities, 're_furniture'); // eslint-disable-line camelcase
 
     if (re_intent) { // eslint-disable-line camelcase
       context.re_intent = re_intent; // eslint-disable-line camelcase
@@ -43,6 +49,56 @@ class Actions {
       context.missing_re_intent = true;
       delete context.re_intent;
     }
+
+    if (re_property) { // eslint-disable-line camelcase
+      context.re_property = re_property; // eslint-disable-line camelcase
+      delete context.missing_re_property;
+    } else {
+      context.missing_re_property = true;
+      delete context.re_property;
+    }
+
+    if (re_location) { // eslint-disable-line camelcase
+      context.re_location = re_location; // eslint-disable-line camelcase
+      delete context.missing_re_location;
+    } else {
+      context.missing_re_location = true;
+      delete context.re_location;
+    }
+
+    if (re_bedrooms) { // eslint-disable-line camelcase
+      context.re_bedrooms = re_bedrooms; // eslint-disable-line camelcase
+      delete context.missing_re_bedrooms;
+    } else {
+      context.missing_re_bedrooms = true;
+      delete context.re_bedrooms;
+    }
+
+    if (re_bathrooms) { // eslint-disable-line camelcase
+      context.re_bathrooms = re_bathrooms; // eslint-disable-line camelcase
+      delete context.missing_re_bathrooms;
+    } else {
+      context.missing_re_bathrooms = true;
+      delete context.re_bathrooms;
+    }
+
+    if (re_furniture) { // eslint-disable-line camelcase
+      context.re_furniture = re_furniture; // eslint-disable-line camelcase
+      delete context.missing_re_furniture;
+    } else {
+      context.missing_re_furniture = true;
+      delete context.re_furniture;
+    }
+
+    if (re_budget) { // eslint-disable-line camelcase
+      context.re_budget = re_budget; // eslint-disable-line camelcase
+      delete context.missing_re_budget;
+    } else {
+      context.missing_re_budget = true;
+      delete context.re_budget;
+    }
+
+
 
     await this.storeContext(sessionId, context);
     return context;
