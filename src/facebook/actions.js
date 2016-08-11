c951f5c16e244f6a5899d7907aa448c78009edef
Alexandru Rosianu <me@aluxian.com>
2016-08-11 17:43:43 +0300

    Fix eslint

--- src/facebook/actions.js
+++ src/facebook/actions.js
@@ -34,71 +34,69 @@ class Actions {
 
   async realEstate({sessionId, context, entities}) {
     console.info('action:realEstate', ...arguments);
-    const re_intent = firstEntityValue(entities, 're_intent'); // eslint-disable-line camelcase
-    const re_property = firstEntityValue(entities, 're_property'); // eslint-disable-line camelcase
-    const re_location = firstEntityValue(entities, 're_location'); // eslint-disable-line camelcase
-    const re_bedrooms = firstEntityValue(entities, 're_bedrooms'); // eslint-disable-line camelcase
-    const re_bathrooms = firstEntityValue(entities, 're_bathrooms'); // eslint-disable-line camelcase
-    const re_budget = firstEntityValue(entities, 're_budget'); // eslint-disable-line camelcase
-    const re_furniture = firstEntityValue(entities, 're_furniture'); // eslint-disable-line camelcase
+    const re_intent = firstEntityValue(entities, 're_intent');
+    const re_property = firstEntityValue(entities, 're_property');
+    const re_location = firstEntityValue(entities, 're_location');
+    const re_bedrooms = firstEntityValue(entities, 're_bedrooms');
+    const re_bathrooms = firstEntityValue(entities, 're_bathrooms');
+    const re_budget = firstEntityValue(entities, 're_budget');
+    const re_furniture = firstEntityValue(entities, 're_furniture');
 
-    if (re_intent) { // eslint-disable-line camelcase
-      context.re_intent = re_intent; // eslint-disable-line camelcase
+    if (re_intent) {
+      context.re_intent = re_intent;
       delete context.missing_re_intent;
     } else {
       context.missing_re_intent = true;
       delete context.re_intent;
     }
 
-    if (re_property) { // eslint-disable-line camelcase
-      context.re_property = re_property; // eslint-disable-line camelcase
+    if (re_property) {
+      context.re_property = re_property;
       delete context.missing_re_property;
     } else {
       context.missing_re_property = true;
       delete context.re_property;
     }
 
-    if (re_location) { // eslint-disable-line camelcase
-      context.re_location = re_location; // eslint-disable-line camelcase
+    if (re_location) {
+      context.re_location = re_location;
       delete context.missing_re_location;
     } else {
       context.missing_re_location = true;
       delete context.re_location;
     }
 
-    if (re_bedrooms) { // eslint-disable-line camelcase
-      context.re_bedrooms = re_bedrooms; // eslint-disable-line camelcase
+    if (re_bedrooms) {
+      context.re_bedrooms = re_bedrooms;
       delete context.missing_re_bedrooms;
     } else {
       context.missing_re_bedrooms = true;
       delete context.re_bedrooms;
     }
 
-    if (re_bathrooms) { // eslint-disable-line camelcase
-      context.re_bathrooms = re_bathrooms; // eslint-disable-line camelcase
+    if (re_bathrooms) {
+      context.re_bathrooms = re_bathrooms;
       delete context.missing_re_bathrooms;
     } else {
       context.missing_re_bathrooms = true;
       delete context.re_bathrooms;
     }
 
-    if (re_furniture) { // eslint-disable-line camelcase
-      context.re_furniture = re_furniture; // eslint-disable-line camelcase
+    if (re_furniture) {
+      context.re_furniture = re_furniture;
       delete context.missing_re_furniture;
     } else {
       context.missing_re_furniture = true;
       delete context.re_furniture;
     }
 
-    if (re_budget) { // eslint-disable-line camelcase
-      context.re_budget = re_budget; // eslint-disable-line camelcase
+    if (re_budget) {
+      context.re_budget = re_budget;
       delete context.missing_re_budget;
     } else {
       context.missing_re_budget = true;
       delete context.re_budget;
     }
-
-
 
     await this.storeContext(sessionId, context);
     return context;
