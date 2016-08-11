99fd9d42262728d7682f8862b23df2215fb4b0e2
Alexandru Rosianu <me@aluxian.com>
2016-08-11 18:59:30 +0300

    Fix real estate

--- src/facebook/actions.js
+++ src/facebook/actions.js
@@ -26,76 +26,41 @@ class Actions {
   async send({sessionId}, {text, quickreplies}) {
     console.info('action:send', ...arguments);
     let msg = new Text(text);
-    if (quickreplies) {
-      console.log('should handle quickreplies', quickreplies);
+    if (Array.isArray(quickreplies)) {
+      for (let quickReply of quickreplies) {
+        msg = msg.addQuickReply(quickReply, quickReply);
+      }
     }
     await this.reply.messages(msg);
   }
 
   async realEstate({sessionId, context, entities}) {
-    console.info('action:realEstate', ...arguments);
-    const re_intent = firstEntityValue(entities, 're_intent');
-    const re_property = firstEntityValue(entities, 're_property');
-    const re_location = firstEntityValue(entities, 're_location');
-    const re_bedrooms = firstEntityValue(entities, 're_bedrooms');
-    const re_bathrooms = firstEntityValue(entities, 're_bathrooms');
-    const re_budget = firstEntityValue(entities, 're_budget');
-    const re_furniture = firstEntityValue(entities, 're_furniture');
+    console.info('action:realEstate', ...arguments, JSON.stringify(entities));
+    const entityNames = [
+      're_intent',
+      're_property',
+      'location',
+      're_num_rooms',
+      're_room_type',
+      're_budget',
+      're_furniture'
+    ];
 
-    if (re_intent) {
-      context.re_intent = re_intent;
-      delete context.missing_re_intent;
-    } else {
-      context.missing_re_intent = true;
-      delete context.re_intent;
+    for (let entityName of entityNames) {
+      const entityValue = firstEntityValue(entities, entityName);
+      if (entityValue) {
+        context[entityName] = entityValue;
+        // delete context['missing_' + entityName];
+      } else {
+        // context['missing_' + entityName] = true;
+        // delete context[entityName];
+      }
     }
 
-    if (re_property) {
-      context.re_property = re_property;
-      delete context.missing_re_property;
-    } else {
-      context.missing_re_property = true;
-      delete context.re_property;
-    }
-
-    if (re_location) {
-      context.re_location = re_location;
-      delete context.missing_re_location;
-    } else {
-      context.missing_re_location = true;
-      delete context.re_location;
-    }
-
-    if (re_bedrooms) {
-      context.re_bedrooms = re_bedrooms;
-      delete context.missing_re_bedrooms;
-    } else {
-      context.missing_re_bedrooms = true;
-      delete context.re_bedrooms;
-    }
-
-    if (re_bathrooms) {
-      context.re_bathrooms = re_bathrooms;
-      delete context.missing_re_bathrooms;
-    } else {
-      context.missing_re_bathrooms = true;
-      delete context.re_bathrooms;
-    }
-
-    if (re_furniture) {
-      context.re_furniture = re_furniture;
-      delete context.missing_re_furniture;
-    } else {
-      context.missing_re_furniture = true;
-      delete context.re_furniture;
-    }
-
-    if (re_budget) {
-      context.re_budget = re_budget;
-      delete context.missing_re_budget;
-    } else {
-      context.missing_re_budget = true;
-      delete context.re_budget;
+    const re_num_rooms = firstEntityValue(entities, 're_num_rooms'); // 5
+    const re_room_type = firstEntityValue(entities, 're_room_type'); // bathroom, bedroom
+    if (re_room_type) {
+      context['re_num_rooms_' + re_room_type] = re_num_rooms || 0;
     }
 
     await this.storeContext(sessionId, context);
