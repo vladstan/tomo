2ad32739c2dc2499816cf0d7ed0629815c72f1dd
Alexandru Rosianu <me@aluxian.com>
2016-08-11 21:18:00 +0300

    Build

--- build/index.js
+++ build/index.js
@@ -3,13 +3,15 @@
 var _sourceMapSupport2 = require('source-map-support');
 
 (0, _sourceMapSupport2.install)();
+console.log('testok');
+
 // import Koa from 'koa';
 
 // const app = new Koa();
 
-throw new Error('ops!l5');
+// throw new Error('ops!l5');
 
 // app.use(async (ctx, next) => {
 //
 // });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFJQSxNQUFNLElBQUksS0FBSixDQUFVLFFBQVYsQ0FBTiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIGltcG9ydCBLb2EgZnJvbSAna29hJztcblxuLy8gY29uc3QgYXBwID0gbmV3IEtvYSgpO1xuXG50aHJvdyBuZXcgRXJyb3IoJ29wcyFsNScpO1xuXG4vLyBhcHAudXNlKGFzeW5jIChjdHgsIG5leHQpID0+IHtcbi8vXG4vLyB9KTtcbiJdfQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLFFBQVEsR0FBUixDQUFZLFFBQVoiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygndGVzdG9rJyk7XG5cbi8vIGltcG9ydCBLb2EgZnJvbSAna29hJztcblxuLy8gY29uc3QgYXBwID0gbmV3IEtvYSgpO1xuXG4vLyB0aHJvdyBuZXcgRXJyb3IoJ29wcyFsNScpO1xuXG4vLyBhcHAudXNlKGFzeW5jIChjdHgsIG5leHQpID0+IHtcbi8vXG4vLyB9KTtcbiJdfQ==
