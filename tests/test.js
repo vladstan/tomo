5eb9cb2b5f907db86252bc46d4761924f26816e0
Alexandru Rosianu <me@aluxian.com>
2016-08-14 10:28:23 +0300

    Add test for GET /facebook

--- tests/test.js
+++ tests/test.js
@@ -10,3 +10,14 @@ test('GET /', async (t) => {
   t.is(res.status, 200);
   t.is(res.text, 'ok');
 });
+
+test('GET /facebook', async (t) => {
+  const challengeString = '123';
+  const res = await request.get('/facebook').query({
+    'hub.mode': 'subscribe',
+    'hub.verify_token': 'EOq3PLzJxg8',
+    'hub.challenge': challengeString,
+  });
+  t.is(res.status, 200);
+  t.is(res.text, challengeString);
+});
