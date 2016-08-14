66158f92da055f804a07d75ebc081de218626bc1
Alexandru Rosianu <me@aluxian.com>
2016-08-14 09:33:27 +0300

    Stream logs from morgan to winston

--- src/middleware/request-logger.js
+++ src/middleware/request-logger.js
@@ -1,9 +1,16 @@
 import {promisify} from 'bluebird';
 import morgan from 'morgan';
 
+import winston from 'winston';
+import winstonStream from 'winston-stream';
+
 export default function() {
   return async(ctx, next) => {
-    const logger = promisify(morgan('common'));
+    const logStream = winstonStream(winston, 'info');
+
+    const format = '[:date[clf]] :method :url :status';
+    const logger = promisify(morgan(format, {stream: logStream}));
+
     await logger(ctx.req, ctx.res);
     await next();
   };
