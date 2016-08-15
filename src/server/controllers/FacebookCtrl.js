c9b9bb750b9a3846f4ded7d346ab7d66540ecde2
Alexandru Rosianu <me@aluxian.com>
2016-08-15 20:39:22 +0300

    Fix undefined err in EventHandler

--- src/server/controllers/FacebookCtrl.js
+++ src/server/controllers/FacebookCtrl.js
@@ -38,7 +38,7 @@ class FacebookCtrl {
     const eventsBySenderId = this.entriesToGroupedEvents(body.entry);
     const tasks = Object.entries(eventsBySenderId)
       .map(async ([senderId, events]) => {
-        await new EventsHandler(this.logger).process(senderId, events);
+        await new EventsHandler(this.config, this.logger).process(senderId, events);
       });
 
     await Promise.all(tasks);
