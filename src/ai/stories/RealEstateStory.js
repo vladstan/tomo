796453532fccd1183c068bc40e9e64474a1b5eee
Vlad Stan <vlad.stan@gmail.com>
2016-08-19 13:26:03 +0300

    Redesigned the onboarding flow, to be more interactive.

--- src/ai/stories/RealEstateStory.js
+++ src/ai/stories/RealEstateStory.js
@@ -59,9 +59,9 @@ class RealEstateStory {
           property_type: context.property_type,
         });
         bot.sendCards(listings); // TODO map every field explicitly
-        const imageProps = {};
-        imageProps.url = 'http://production.kyero.s3.amazonaws.com/3648/3648907/vwxrvxfy2d_long_term_rent_palma%20%2819%29.jpg';
-        bot.sendImage(imageProps);
+        // const imageProps = {};
+        // imageProps.url = 'http://production.kyero.s3.amazonaws.com/3648/3648907/vwxrvxfy2d_long_term_rent_palma%20%2819%29.jpg';
+        // bot.sendImage(imageProps);
         return true;
       }
 
