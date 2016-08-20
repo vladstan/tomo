80050925d52365b9079544b2bc9c27dfc5a1111c
Vlad Stan <vlad.stan@gmail.com>
2016-08-20 12:23:40 +0300

    Started the Expedia API for "Activities" in Palma and worldwide.

---
+++ src/apis/ExpediaApi.js
@@ -0,0 +1,35 @@
+
+import Config from 'server/Config';
+
+class ExpediaApi {
+
+  static _instance: ExpediaApi;
+
+  client: ExpediaApi;
+  config: Config;
+
+  static getInstance(config) {
+    if (!this._instance) {
+      this._instance = new this(config);
+    }
+
+    return this._instance;
+  }
+
+  constructor(config: Config) {
+    const key: string = config.expediaApiKey;
+    this.client = new ExpediaApi(key); // TODO not sure about this one
+  }
+
+  async getActivities({location, category}) {
+    return await this.client
+      .location(location)
+      .category(category)
+      // .exclude('minutely,daily')
+      // .extendHourly(true)
+      .get();
+  }
+
+}
+
+export default ExpediaApi;
