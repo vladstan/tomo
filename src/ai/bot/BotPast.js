b19ab01c863da5d40eb6fce6d2c2944726d93cbf
Alexandru Rosianu <me@aluxian.com>
2016-10-24 10:54:27 +0100

    Fix botPast sorting

--- src/ai/bot/BotPast.js
+++ src/ai/bot/BotPast.js
@@ -6,24 +6,39 @@ class BotPast {
   }
 
   botAsked(intent) {
+    // function lastMsgs(arr, num) {
+    //   const newArr = [];
+    //   let startIndex = arr.length - num;
+    //   if (startIndex < 0) {
+    //     startIndex = 0;
+    //   }
+    //   for (let i = startIndex; i < arr.length; i++) {
+    //     newArr.push(arr[i]);
+    //   }
+    //   return newArr;
+    // }
+
     // TODO: search only the last 5 messages
-    console.log('messages=', this.messages);
-    console.log('newMessages=', this.newMessages);
+    // console.log('messages=', lastMsgs(this.messages, 5));
+    // console.log('newMessages=', lastMsgs(this.newMessages, 5));
     const previousMessagesFromBot = this.messages
       .concat(this.newMessages)
-      .filter((msg) => msg.senderType === 'bot')
-      .sort((a, b) => {
-        if (a.createdAt < b.createdAt) {
-          return -1;
-        } else if (a.createdAt < b.createdAt) {
-          return +1;
-        } else {
-          return 0;
-        }
-      });
+      .filter((msg) => msg.senderType === 'bot');
+      // TODO: fix this sort (it might work without it as well)
+      // .sort((a, b) => {
+      //   if (a.createdAt < b.createdAt) {
+      //     return -1;
+      //   } else if (a.createdAt < b.createdAt) {
+      //     return +1;
+      //   } else {
+      //     return 0;
+      //   }
+      // });
 
     if (previousMessagesFromBot.length) {
       const previousMessageFromBot = previousMessagesFromBot[previousMessagesFromBot.length - 1];
+      // console.log('previousMessageFromBot:', previousMessageFromBot);
+      // console.log('previousMessageFromBot.entities.intent', previousMessageFromBot.entities.intent, 'intent', intent);
       return previousMessageFromBot.entities.intent === intent;
     }
 
